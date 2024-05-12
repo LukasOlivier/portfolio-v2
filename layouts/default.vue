c4e5301efd22eaa659df728d7fe84050bd9faa0b
Lukas Olivier <olivier.lukas2003@gmail.com>
2024-05-12 21:19:49 +0200

    feat: added vercel speed insights

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,9 @@
+<script setup>
+import { SpeedInsights } from '@vercel/speed-insights/nuxt'
+</script>
+
 <template>
+  <SpeedInsights />
   <div class="gd-container font-spacegrotesk">
     <header class="fixed w-full bg-[#F1F2F4] dark:bg-slate-950 z-10">
       <MainHeader />
