56eb16d924c3eba1ec0fa76d557fa804f356f0e7
Lukas Olivier <olivier.lukas2003@gmail.com>
2025-01-18 21:42:43 +0100

    chore: updated about me text

--- components/main/about.vue
+++ components/main/about.vue
@@ -4,52 +4,47 @@
          <h2
             class="mb-5 text-4xl font-semibold leading-tight text-black dark:text-white md:text-5xl"
          >
-            About me
+            About Me
          </h2>
          <p>
-            As a {{ new Date().getFullYear() - 2003 }}-year-old software
-            engineering student, I have a deep passion for programming and
-            technology. Besides my studies, I actively contribute to a youth
-            movement, and I find creative expression through drumming in my free
-            time. <br /><br />
+            Hi there! 👋 I'm a {{ new Date().getFullYear() - 2003 }}-year-old
+            <strong>civil engineering student at UGent</strong> with a passion
+            for technology . Alongside my studies, I've started my journey as a
+            <strong>student freelancer</strong>, specializing in software
+            development.
+         </p>
+         <br />
+         <p>
+            <strong>🚀 What I Do</strong><br />
+            I help individuals and businesses bring their ideas to life. Whether
+            it's building a sleek <strong>website</strong>,
+            <strong> progressive web app</strong>, or a
+            <strong>RESTful API</strong>, I can help you create a digital
+            solution that meets your needs.
+         </p>
 
-            This year marks the final year in the bachelor's program in software
-            engineering at Howest Brugge. I'm a fast learner, always eager to
-            acquire new knowledge. While I thrive in team environments and am
-            ready to assist others, I am equally capable of working
-            independently. As a perfectionist, I strive to deliver the best
-            possible results.<br /><br />
-
-            I am currently learning
-            <a
-               class="underline"
-               aria-label="Nuxt 3"
-               target="_blank"
-               href="https://nuxt.com/"
-               >Nuxt 3</a
-            >
-            and
-            <a
-               class="underline"
-               target="_blank"
-               aria-label="Tailwind CSS"
-               href="https://tailwindcss.com/"
-               >Tailwind CSS</a
-            >
-            to create websites like this one. Find all my projects on
+         <br />
+         <p>
+            <strong>🌟 Beyond Work</strong><br />
+            When I'm not coding, you'll find me contributing to a
+            <strong>youth movement</strong>, exploring my creative side through
+            , or learning new skills to push myself further.
+         </p>
+         <br />
+         <p>
+            <strong>🌐 Let's Build Something Together</strong><br />
+            Ready to turn your ideas into reality? Check out
             <NuxtLink
                aria-label="Projects Page"
                class="underline"
                href="/projects"
             >
-               my projects page.
+               my Projects page
             </NuxtLink>
-            Or read about my experiences as a software engineering student on
-            <NuxtLink aria-label="Blogs Page" class="underline" href="/blogs">
-               my blog.
-            </NuxtLink>
-            <br /><br />
+            to see what I've worked on. <br />
+            Let's create something amazing!
          </p>
+         <br />
          <h2
             class="text-xl font-semibold leading-tight text-black dark:text-white"
          >
