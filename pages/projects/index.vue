788ba562a2180448c9176a67483829ea3bf55458
Lukas Olivier <olivier.lukas2003@gmail.com>
2025-10-21 21:10:45 +0200

    chore: added new project

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -10,11 +10,12 @@
 		{
 			id: 17,
 			title: 'Route Random - Running Route Generator',
-			description: 'Route Random is a smart running route generator that helps you discover new paths for running, jogging, or walking - wherever you are. Instantly create random, circular routes tailored to your chosen distance and location. Search by address or click on the map to set your starting point, then fine-tune your route interactively. Export your routes to GPX or GeoJSON for use with Garmin, Strava, or any fitness app. No accounts, no tracking—just fresh routes, every time. Powered by OpenStreetMap, Leaflet, and OpenRouteService',
+			description:
+				'Route Random is a smart running route generator that helps you discover new paths for running, jogging, or walking - wherever you are. Instantly create random, circular routes tailored to your chosen distance and location. Search by address or click on the map to set your starting point, then fine-tune your route interactively. Export your routes to GPX or GeoJSON for use with Garmin, Strava, or any fitness app. No accounts, no tracking—just fresh routes, every time. Powered by OpenStreetMap, Leaflet, and OpenRouteService',
 			category: 'Personal',
 			image: '/projects-img/route-random.png',
 			tags: [
-				'devicon-plain:nuxtjs',
+				'devicon-plain:nextjs',
 				'simple-icons:tailwindcss',
 				'devicon-plain:typescript',
 			],
